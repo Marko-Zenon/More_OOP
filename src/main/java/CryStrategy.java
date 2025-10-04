ba515e65ed7216d417f1a85c1972234949c959bd
Mar4ik <peleshko.pn@ucu.edu.ua>
2025-10-04 22:55:22 +0300

    Finall

--- src/main/java/CryStrategy.java
+++ src/main/java/CryStrategy.java
@@ -1,6 +1,6 @@
 public class CryStrategy implements KickStrategy {
     @Override
     public void kick(Character self, Character enemy) {
-        System.out.println("😭 Hobbit cries!");
+        System.out.println("Hobbit cries!");
     }
 }
